9b5dfed23a21cd01293aa5529f3dc5cbfc501bac
donghwi <ehdehd1289@naver.com>
2020-10-28 01:11:11 +0900

    Notice sideProject start => NoticeList comp -> veiw -> develop

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Redirect } from "react-router-dom";
+
+const NoticeList = React.lazy(() => import("./components/NoticeList"));
+const Join = React.lazy(() => import("./components/Join"));
+const Login = React.lazy(() => import("./components/Login"));
+
+const routes = [
+  {
+    path: "/noticeList",
+    exact: true,
+    component: () => <Redirect to="/noticeList"/>
+  },
+  {
+    path: "/noticeList",
+    exact: true,
+    component: NoticeList
+  },
+  {
+    path: "/join",
+    component: Join
+  },
+  {
+    path: "/login",
+    component: Login
+  },
+];
+
+export default routes;
